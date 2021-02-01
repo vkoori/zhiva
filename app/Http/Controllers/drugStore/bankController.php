965d76114bf6440d2e546b7c6475aeb4f18108d7
vkoori <kooroshsafeashrafi@yahoo.com>
2021-02-01 17:25:31 +0330

    bankController has been changed

--- app/Http/Controllers/drugStore/bankController.php
+++ app/Http/Controllers/drugStore/bankController.php
@@ -16,8 +16,13 @@ class bankController
 		if ($_POST["cart"] != $cart) 
 			return redirect('cart')->withErrors(['سبد خرید شما تغییر یافته است.']);
 
+		$cartController = app('App\Http\Controllers\drugStore\cartController');
+		$data = $cartController->calcCart();
+		if (sizeof($data['cart']) == 0) 
+			return redirect('cart');
+
 		// use class discountController
-		
 		dd($_POST);
+		dd($data);
 	}
 }
